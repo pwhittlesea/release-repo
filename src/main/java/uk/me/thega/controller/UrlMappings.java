1fd693023761eb2a4f116308ea6ccd2b478a36be
Phillip Whittlesea <pw.github@thega.me.uk>
2014-10-12 09:01:49 +0100

    Add a basic mechanism for listing resources without HTML

--- src/main/java/uk/me/thega/controller/UrlMappings.java
+++ src/main/java/uk/me/thega/controller/UrlMappings.java
@@ -13,6 +13,7 @@ final class UrlMappings {
 	static final String ROOT_BROWSE = "/browse";
 	static final String ROOT_DOWNLOAD = "/download";
 	static final String ROOT_INDEX = "/";
+	static final String ROOT_LIST = "/list";
 	static final String ROOT_UPLOAD = "/upload";
 	static final String VERSION = "/{family}/{application}/{version}";
 	static final String VERSION_DOWNLOAD = "/{family}/{application}/{version}.{extension}";
